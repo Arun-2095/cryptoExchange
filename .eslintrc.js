367b9b80b39e63ab1324cec265f785fedaea32bf
arunkumar <arunagiri2195@gmail.com>
2023-08-13 13:52:16 +0530

    Initial setup & eslint , husky , path alias configuration

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  settings: {
+    react: {
+      version: "18.2",
+    },
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}", "*.tsx", "*.ts"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2],
+  },
+  ignorePatterns: ["reportWebVitals.ts", ".eslintrc.js", "craco.config.js"],
+};
