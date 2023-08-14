52695a5ea9d32d4fef0f7786ff03298618098aca
arunkumar <arunagiri2195@gmail.com>
2023-08-14 10:40:18 +0530

    bootstrap , font intialisation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import './assets/css/variable.css'
 import reportWebVitals from './reportWebVitals'
 import { AppRoutes } from './Router'
 import {
